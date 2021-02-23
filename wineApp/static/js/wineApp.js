7a8135a719ff8d6a0ca7c9b3ed1b5f5df3b11b83
Crystal Aumand <otheduck@hotmail.com>
2021-02-23 09:30:07 -0800

    Fade working on scroll

--- wineApp/static/js/wineApp.js
+++ wineApp/static/js/wineApp.js
@@ -29,13 +29,17 @@
 //     })
 // });
 $(document).ready(function(){
+    // $(".parallax").animate({'opacity':'1'},1000);       
     $(window).scroll(function(){
-        console.log("fade in working")
-        $('.grape_img').each( function (i){
-            $(this).animate({'opacity':'1'},1500);
-        })
-    })
-})
-
+        $('.main_box').each( function (i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 250;
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},1000); 
+            }; 
+        });
+    });
+});
 
 

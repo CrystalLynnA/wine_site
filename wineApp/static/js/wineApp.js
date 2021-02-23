0e31a2c38b585a8169f8a2d7b7cc453f79cd4a77
Crystal Aumand <otheduck@hotmail.com>
2021-02-22 17:35:26 -0800

    JavaScript updates

--- wineApp/static/js/wineApp.js
+++ wineApp/static/js/wineApp.js
@@ -1,24 +1,41 @@
+// $(document).ready(function(){
+//     // Activate Carousel
+//     $("#myCarousel").carousel();
+//     // Enable Carousel Indicators
+//     $(".item1").click(function(){
+//         $("#myCarousel").carousel(0);
+//     });
+//     $(".item2").click(function(){
+//         $("#myCarousel").carousel(1);
+//     });
+//     $(".item3").click(function(){
+//         $("#myCarousel").carousel(2);
+//     });
+//     $(".item4").click(function(){
+//         $("#myCarousel").carousel(3);
+//     });
+//     // Enable Carousel Controls
+//     $(".left").click(function(){
+//         $("#myCarousel").carousel("prev");
+//     });
+//     $(".right").click(function(){
+//         $("#myCarousel").carousel("next");
+//     });
+// });
+// $(document).ready(function(){
+//     $('button').click(function(){
+//         console.log("WORKING JS")
+//         alert("You have clicked a button!");
+//     })
+// });
 $(document).ready(function(){
-    // Activate Carousel
-    $("#myCarousel").carousel();
-    // Enable Carousel Indicators
-    $(".item1").click(function(){
-        $("#myCarousel").carousel(0);
-    });
-    $(".item2").click(function(){
-        $("#myCarousel").carousel(1);
-    });
-    $(".item3").click(function(){
-        $("#myCarousel").carousel(2);
-    });
-    $(".item4").click(function(){
-        $("#myCarousel").carousel(3);
-    });
-    // Enable Carousel Controls
-    $(".left").click(function(){
-        $("#myCarousel").carousel("prev");
-    });
-    $(".right").click(function(){
-        $("#myCarousel").carousel("next");
-    });
-});
+    $(window).scroll(function(){
+        console.log("fade in working")
+        $('.grape_img').each( function (i){
+            $(this).animate({'opacity':'1'},1500);
+        })
+    })
+})
+
+
+

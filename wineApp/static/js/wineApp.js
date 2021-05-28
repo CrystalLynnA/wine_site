1937d4d6db10d1543f90844b54cee540104a9bed
Crystal <crystal.lynn.aumand@gmail.com>
2021-05-28 10:37:22 -0700

    update

--- wineApp/static/js/wineApp.js
+++ wineApp/static/js/wineApp.js
@@ -1,22 +1,4 @@
 $(document).ready(function(){
-    var modal = document.getElementById('bottle1');
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-    // Get the modal
-    var modal = document.getElementById('id02');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-});
-$(document).ready(function(){
     // $(".parallax").animate({'opacity':'1'},1000);       
     $(window).scroll(function(){
         $('.main_box').each( function (i){
@@ -28,6 +10,30 @@ $(document).ready(function(){
             }; 
         });
     });
+// Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }}
 });
 
 
